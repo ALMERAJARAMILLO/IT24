3a0c8e5de07a91355ee388473c4805c3adaf6b6f
ALMERAJARAMILLO <jramilloalmera92@gmail.com>
2024-10-19 01:55:22 +1100

    Added class Student list

---
+++ applet-4.js
@@ -0,0 +1,13 @@
+class StudentList {
+
+    constructor(dataUrl) {
+        this.dataUrl = dataUrl;
+        this.students = [];
+        this.init();
+    }
+    async init() {
+        await this.fetchData();
+        this.renderStudentList(this.students);
+        this.bindSearchEvent();
+    }
+}
